7db925f1a294d8e6148bac22aedb343ab27e8729
mtalal0786 <talalshahbaz000786@gmail.com>
2025-09-30 12:10:46 +0500

    user controlller and routes created

--- src/app.js
+++ src/app.js
@@ -17,4 +17,11 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// Routes
+import userRoutes from './routes/user.routes.js';
+
+// Routes Declaration
+app.use('/api/v1/users', userRoutes);
+
 export { app };
